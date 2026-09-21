619d90f6d126eeed84c6d04d71e03a93ec28f236
Methasha20250805@iit.ac.lk <methashag@gmail.com>
2026-09-21 09:22:30 +0530

    editing comments

--- src/main/java/com/example/votingsystem/Ballot.java
+++ src/main/java/com/example/votingsystem/Ballot.java
@@ -34,7 +34,7 @@ public class Ballot {
     public void setVoterId(String v) { voterId.set(v); }
     public StringProperty voterIdProperty() { return voterId; }
 
-    //  candidateId
+    //  getters and setters for the candidateID
     public String getCandidateId() { return candidateId.get(); }
     public void setCandidateId(String v) { candidateId.set(v); }
     public StringProperty candidateIdProperty() { return candidateId; }
